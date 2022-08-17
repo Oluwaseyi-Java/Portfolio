70f6b075429fe542867bde368a072f32c3c15f4f
Adesunloro Oluwaseyi <amiltontech.co@gmail.com>
2022-08-17 22:23:22 +0100

    More features

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -2,14 +2,23 @@ import React, { useEffect, useState } from 'react'
 import Title from '../Title'
 import Project from './Project'
 import { ProjectData } from "../../UiData.js"
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
 
 const Projects = () => {
 
     const [state, setState] = useState([])
+    const [isMore, setIsMore] = useState(false)
 
     useEffect(() => {
-        setState(ProjectData)
-    }, [])
+        if (isMore) {
+
+            setState(ProjectData)
+        }
+        else {
+            setState(ProjectData.slice(0, 3))
+        }
+
+    }, [isMore])
 
     const projects = state.map((project, index) => {
         return (
@@ -30,11 +39,18 @@ const Projects = () => {
     return (
         <div className='Projects' id='projects'>
             <Title title="Projects" />
-           
-                <p className='Head'>My Latest Work</p>
-           
+
+            <p className='Head'>My Latest Work</p>
+
             <div className='project-display'>
                 {projects}
+            </div>
+            <div className='Show'>
+                <p
+                    onClick={() => {
+                        setIsMore(!isMore)
+                    }}
+                >{isMore ? "Show Less" : "Show More"}{isMore ? <FaChevronUp /> : <FaChevronDown />}</p>
             </div>
         </div>
     )
